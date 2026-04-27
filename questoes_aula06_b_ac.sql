8c9024b77c8860fcc4deb4e650eb6ced5f1ed9f1
motarenan-art <mota.renan@aluno.ifsp.edu.br>
2026-04-27 18:26:08 -0300

    Update SQL permissions for users and queries

--- questoes_aula06_b_ac.sql
+++ questoes_aula06_b_ac.sql
@@ -1,42 +1,42 @@
 -- Questão 01. O User_B poderá selecionar todos os atributos da relação 
 -- INSTRUCTOR e TAKES, exceto salary e grade, respectivamente.
-GRANT SELECT ON dbo.instructor TO User_B;
-DENY SELECT ON dbo.instructor(salary) TO User_B;
+GRANT SELECT ON instructor TO User_B;
+DENY SELECT ON instructor(salary) TO User_B;
 
-GRANT SELECT ON dbo.takes TO User_B;
-DENY SELECT ON dbo.takes(grade) TO User_B;
+GRANT SELECT ON takes TO User_B;
+DENY SELECT ON takes(grade) TO User_B;
 
 
 -- Questão 02. O User_C poderá selecionar ou modificar a relação 
 -- SECTION, mas só poderá recuperar e modificar os atributos 
 -- course_id, sec_id, semester e year.
-GRANT SELECT ON dbo.section (course_id, sec_id, semester, year) TO User_C;
+GRANT SELECT ON section (course_id, sec_id, semester, year) TO User_C;
 
-GRANT UPDATE ON dbo.section (course_id, sec_id, semester, year) TO User_C;
+GRANT UPDATE ON section (course_id, sec_id, semester, year) TO User_C;
 
 
 -- Questão 03. O User_D poderá selecionar qualquer atributo das 
 -- relações INSTRUCTOR e STUDENT. Poderá selecionar os atributos 
 -- da view grade_points.
-GRANT SELECT ON dbo.instructor TO User_D;
-GRANT SELECT ON dbo.student TO User_D;
-GRANT SELECT ON dbo.grade_points TO User_D;
+GRANT SELECT ON instructor TO User_D;
+GRANT SELECT ON student TO User_D;
+GRANT SELECT ON grade_points TO User_D;
 
 
 -- Questão 04. O User_E poderá selecionar qualquer atributo de STUDENT,
 -- mas somente para tuplas de STUDENT que tem dept_name = 'Civil Eng.'
 
--- CREATE OR ALTER VIEW dbo.vw_student_civil_eng AS
--- SELECT * FROM dbo.student 
+-- CREATE OR ALTER VIEW vw_student_civil_eng AS
+-- SELECT * FROM student 
 -- WHERE dept_name = 'Civil Eng.';
 
 
-GRANT SELECT ON dbo.vw_student_civil_eng TO User_E;
+GRANT SELECT ON vw_student_civil_eng TO User_E;
 
 
 -- Questão 05. Revoque os privilégios do usuário User_E
 -- Revogando a permissão que foi concedida na questão 04
-REVOKE SELECT ON dbo.vw_student_civil_eng FROM User_E;
+REVOKE SELECT ON vw_student_civil_eng FROM User_E;
 
 
 -- Questão 06. Mostre os privilégios concedidos aos usuários 'User_A',
